d8a5a4ac8b5cd0fd68dc4f8950f7d4afcec1c91e
Maarit Välisuo <mvalisuo@LUWFVFL50WT1WGC.local>
2024-10-03 12:04:59 +0300

    Lisätty python pyöristysesimerkki.

---
+++ 4luento/round_with_python.py
@@ -0,0 +1,15 @@
+from math import *
+
+a = 34.22  # 4 merkitsevää numeroa, 2 desimaalia
+b = 91.76627773  #10 merkitsevää numeroa
+c = a * b
+precision = 4
+
+rounded_c = '{:g}'.format(float('{:.{p}g}'.format(c, p=precision)))
+print(
+    f"c pyöristettynä 4:n merkitsevän numeron tarkkuudella is {rounded_c} its type is {type(c)}"
+)
+print('{:g}'.format(float('{:.{p}g}'.format(c, p=4))))
+
+d = a + b
+print(f"Tulosta kahden desimaalin tarkkuudella {round(d, 2)}")
